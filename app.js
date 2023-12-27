10371638825712c38f336f277cdfb0e8402c2521
Felix <felixsavero78@gmail.com>
2023-12-27 16:49:31 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+// Routers
+const categoriesRouter = require('./app/api/v1/categories/router');
+const v1 = '/api/v1/cms';
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(v1, categoriesRouter);
+
+module.exports = app;
