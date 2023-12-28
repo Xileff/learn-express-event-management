a8dcecf720adc92994eebd76650bcf9b336a4bf3
Felix <felixsavero78@gmail.com>
2023-12-28 16:49:12 +0700

    feat: finish crud for Category

---
+++ app/api/v1/categories/controller.js
@@ -0,0 +1,88 @@
+const Categories = require('./model');
+
+const create = async(req, res, next) => {
+  try {
+    const { name } = req.body;
+    const result = await Categories.create({ name });
+    return res.status(201).json({
+      status: 'success',
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+const index = async(req, res, next) => {
+  try {
+    const result = await Categories.find().select('_id name');
+    return res.status(200).json({
+      status: 'success',
+      data: result,
+    })
+  } catch (err) {
+    next(err);
+  }
+}
+
+const find = async(req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Categories.findOne({ _id: id }).select('_id name');
+
+    if (!result) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'category not found',
+      });
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+const update = async(req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const result = await Categories.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true, runValidators: true },
+    );
+
+    return res.status(200).json({
+      status: 'success',
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+const destroy = async(req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Categories.findByIdAndDelete(id);
+    return res.status(200).json({
+      status: 'success',
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { 
+  create, 
+  index,
+  find,
+  update,
+  destroy,
+};
---
+++ app/api/v1/categories/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { model, Schema } = mongoose;
+
+const categorySchema = Schema(
+  {
+    name: {
+      type: String,
+      minlength: [3, 'Min length for category name is 3 characters'],
+      maxlength: [20, 'Max length for category name is 20 characters'],
+      required: [true, 'Category name is mandatory']
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model('Category', categorySchema);
